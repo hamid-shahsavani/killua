4d27eee8fd975b96f98c8f459c0216e042191660
sys113 <051.sys113@gmail.com>
2023-09-03 22:08:27 +0330

    delete useSetKillua and useGetKillua

--- index.ts
+++ index.ts
@@ -1,4 +1,2 @@
 export { default as useKillua } from './hooks/killua';
-export { default as useGetKillua } from './hooks/getKillua';
-export { default as useSetKillua } from './hooks/setKillua';
 export { default as thunder } from './functions/thunder';
