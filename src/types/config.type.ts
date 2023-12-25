46d6a416e561ae5017edb5ccb53e8aa3babc1e88
sys113 <24222077+sys113@users.noreply.github.com>
2023-12-25 03:40:40 +0330

    chore: extract all slice config properties and add type to `config.type.ts`

--- src/types/config.type.ts
+++ src/types/config.type.ts
@@ -1,21 +1,37 @@
+export type TKey = string;
+export type TEncrypt = boolean | undefined;
+export type TExpire = string | undefined;
+export type TSchema =
+  | { parse: (val: any) => any }
+  | { validateSync: (val: any) => any | undefined }
+  | undefined;
+export type TReducers<TSlice> =
+  | Record<string, (value: TSlice, payload?: any) => TSlice>
+  | undefined;
+export type TSelectors<TSlice> =
+  | Record<string, (value: TSlice, payload?: any) => any>
+  | undefined;
+export type TEvents<TSlice> = Partial<
+  Record<'onChange' | 'onExpire', (value: TSlice) => void>
+>;
+export type TSSR<Type> = Type extends true ? true : false | undefined;
+
 export type TConfig<TSlice> = {
-  key: string;
-  encrypt?: boolean;
-  expire?: string;
-  schema?:
-    | { parse: (val: TSlice) => TSlice }
-    | { validateSync: (val: TSlice) => TSlice | undefined };
-  reducers?: Record<string, (value: TSlice, payload?: any) => TSlice>;
-  selectors?: Record<string, (value: TSlice, payload?: any) => any>;
-  events?: Partial<Record<'onChange' | 'onExpire', (value: TSlice) => void>>;
+  key: TKey;
+  encrypt?: TEncrypt;
+  expire?: TExpire;
+  schema?: TSchema;
+  reducers?: TReducers<TSlice>;
+  selectors?: TSelectors<TSlice>;
+  events?: TEvents<TSlice>;
 } & (
   | {
-      ssr: true;
+      ssr: TSSR<true>;
       defaultClient: TSlice;
       defaultServer: TSlice;
     }
   | {
-      ssr?: false;
+      ssr?: TSSR<false>;
       default: TSlice;
     }
 );
