3ca19d5a71adf417323ff2deb6d7fcd19ebe73d6
sys113 <051.sys113@gmail.com>
2024-02-25 01:23:03 +0330

    chore: delete old tests

--- tests/generate-slice-key-name.util.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, it } from 'vitest';
-import generateSliceStorageKey from '../src/utils/other/generate-slice-storage-key.util';
-
-describe('generate-slice-key-name.util.ts', (): void => {
-  it('should generate the correct slice key name', (): void => {
-    expect(generateSliceStorageKey({key: 'counter'})).toBe('slice-counter');
-  });
-});
